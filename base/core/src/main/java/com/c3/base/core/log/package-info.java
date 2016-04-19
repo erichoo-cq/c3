991ff37b1f76555cbf30700fc637d583983ff215
cqccc <cqccc123@qq.com>
2016-04-19 15:27:31 +0800

    add core project

---
+++ base/core/src/main/java/com/c3/base/core/log/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * cqccc package-info.java 2016年4月19日 下午3:15:49
+ 
+ * @Copyright: Copyright 2015 www.cqccc.com.cn, Use is subject to license terms.
+ * @Company: 重庆通信建设有限责任公司
+ */
+/**description:
+ *
+ * @version 2016年4月19日 下午3:15:49
+ * @see
+ * modify content------------author------------date
+ */
+package main.java.com.c3.base.core.log;
---
+++ base/core/src/main/java/com/c3/base/core/repository/hibernate/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * cqccc package-info.java 2016年4月19日 下午3:21:47
+ 
+ * @Copyright: Copyright 2015 www.cqccc.com.cn, Use is subject to license terms.
+ * @Company: 重庆通信建设有限责任公司
+ */
+/**description:
+ *
+ * @version 2016年4月19日 下午3:21:47
+ * @see
+ * modify content------------author------------date
+ */
+package main.java.com.c3.base.core.repository.hibernate;
