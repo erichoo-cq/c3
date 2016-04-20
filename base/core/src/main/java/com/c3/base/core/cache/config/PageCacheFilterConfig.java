5abec00fb3103b249b5392216a8abeeadb8dd795
heshan07 <heshan07@foxmail.com>
2016-04-20 16:12:55 +0800

    二级缓存，查询缓存，页面缓存

---
+++ base/core/src/main/java/com/c3/base/core/cache/config/PageCacheFilterConfig.java
@@ -0,0 +1,26 @@
+package com.c3.base.core.cache.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.c3.base.core.cache.web.PageCacheFilter;
+
+public class PageCacheFilterConfig {
+
+   private @Autowired AutowireCapableBeanFactory beanFactory;
+
+   @Bean
+   public FilterRegistrationBean myFilter() {
+      FilterRegistrationBean registration = new FilterRegistrationBean();
+      PageCacheFilter pageCacheFilter = new PageCacheFilter();
+      beanFactory.autowireBean(pageCacheFilter);
+      registration.setFilter(pageCacheFilter);
+      
+      
+      
+      registration.addUrlPatterns("/myfitlerpath/*");
+      return registration;
+   }
+}
