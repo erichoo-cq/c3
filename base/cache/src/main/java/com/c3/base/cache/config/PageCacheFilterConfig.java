72361a6096f39d997f20eeb027737b5763da4c49
EthanHe1989 <641820828@qq.com>
2016-04-22 11:55:59 +0800

    添加页面缓存开关配置

--- base/cache/src/main/java/com/c3/base/cache/config/PageCacheFilterConfig.java
+++ base/cache/src/main/java/com/c3/base/cache/config/PageCacheFilterConfig.java
@@ -1,6 +1,16 @@
 package com.c3.base.cache.config;
 
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
@@ -20,19 +30,40 @@ import com.c3.base.cache.web.PageCacheFilter;
 @Configuration
 public class PageCacheFilterConfig {
 
+   @Value("${c3_cache.pagecache.enable:false}")
+   private boolean enable;
+
+   @Value("${c3_cache.pagecache.packagescan:}")
+   private String packagescan;
+
    @Autowired
    private AutowireCapableBeanFactory beanFactory;
 
    @Bean
    public FilterRegistrationBean registFilter() {
       FilterRegistrationBean registration = new FilterRegistrationBean();
-      PageCacheFilter pageCacheFilter = new PageCacheFilter();
-      EnablePageCacheScanner scanner = new EnablePageCacheScanner();
-      scanner.scan(pageCacheFilter, "com.c3.**.pagecache");
-      beanFactory.autowireBean(pageCacheFilter);
-      registration.setFilter(pageCacheFilter);
-      registration.addUrlPatterns(pageCacheFilter.getIncludePages());
-      registration.addUrlPatterns(pageCacheFilter.getIncludePatterns());
+      if (enable) {
+         PageCacheFilter pageCacheFilter = new PageCacheFilter();
+         EnablePageCacheScanner scanner = new EnablePageCacheScanner();
+         scanner.scan(pageCacheFilter, packagescan.split(","));
+         beanFactory.autowireBean(pageCacheFilter);
+         registration.setFilter(pageCacheFilter);
+         registration.addUrlPatterns(pageCacheFilter.getIncludePages());
+         registration.addUrlPatterns(pageCacheFilter.getIncludePatterns());
+      } else {
+         registration.setFilter(new Filter() {
+            @Override
+            public void init(FilterConfig filterConfig) throws ServletException {
+            }
+            @Override
+            public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+                  throws IOException, ServletException {
+            }
+            @Override
+            public void destroy() {
+            }
+         });
+      }
       return registration;
    }
 }
